42a7392967719fe08ba2a83d2ff2e8b86fd1834e
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-07-29 16:00:15 +0800

    table cloth cutout

---
+++ src/main/java/dev/xkmc/gensokyolegacy/content/ui/ConversationScreen.java
@@ -0,0 +1,12 @@
+package dev.xkmc.gensokyolegacy.content.ui;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class ConversationScreen extends Screen {
+
+	protected ConversationScreen(Component title) {
+		super(title);
+	}
+
+}
